942719307c3894c1d9aaae3f31b90f283e485998
Musadrammeh <43375006+Musadrammeh@users.noreply.github.com>
2021-06-08 19:09:59 +0200

    DateUpdate

--- app/src/main/java/com/example/uipractice/MainActivity.kt
+++ app/src/main/java/com/example/uipractice/MainActivity.kt
@@ -2,11 +2,37 @@ package com.example.uipractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    private var buttonSignup: Button? = null
+    private var buttonLogin: Button? = null
+    private var textViewTime: TextView? = null
+    private var textViewDate: TextView? = null
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textViewDate = findViewById(R.id.dateTextView)
+        setDateAndTime()
+
+
+    }
+
+    private fun setDateAndTime(){
+        val today = Date()
+        val dateFormat = SimpleDateFormat("dd MMMM yyyy")
+        val formatedDate = dateFormat.format(today)
+        Log.i("TAG", "$formatedDate")
+
+        textViewDate?.text = formatedDate
     }
 }
