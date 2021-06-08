7b5b70d198122014c2e4a074bc6ddc15d404fed7
Musadrammeh <43375006+Musadrammeh@users.noreply.github.com>
2021-06-08 16:02:15 +0200

    ButtonForDateAndTime

--- app/src/main/java/com/example/uipractice/LogInActivity.kt
+++ app/src/main/java/com/example/uipractice/LogInActivity.kt
@@ -3,9 +3,13 @@ package com.example.uipractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+
 class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+
     }
 }
